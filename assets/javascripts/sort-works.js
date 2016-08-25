a143c7b1ec782a91416fe8ed52a2bf397dc2dcb2
Scott Blumenthal <scott.blumenthal@gmail.com>
2016-08-25 16:59:41 -0400

    Update sort-works.js
    remove debugs

--- assets/javascripts/sort-works.js
+++ assets/javascripts/sort-works.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
     $works = $('.works-maj, #works-list > div'),
     $container = $works.eq(0).parent();
     
-    console.log(WORKS)
+    //console.log(WORKS)
     WORKS.sort(function(a, b) {
       return (Date.parse(new Date(a.date)) > Date.parse(new Date(b.date))) ? 1 : -1;
     })
-    console.log(WORKS)
+    //console.log(WORKS)
     WORKS.forEach(function(work) {
       $container.prepend($works.filter('[title="' + work.title + '"]'));
-      console.log(work, $works.filter('[title="' + work.title + '"]'))
+      //console.log(work, $works.filter('[title="' + work.title + '"]'))
     });
 });
