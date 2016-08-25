ae367ef9eab7fb70bf0e0252ec1f08c4f225e4a7
Scott Blumenthal <scott.blumenthal@gmail.com>
2016-08-25 16:53:09 -0400

    another test

--- assets/javascripts/sort-works.js
+++ assets/javascripts/sort-works.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
     $works = $('.works-maj, #works-list > div'),
     $container = $works.eq(0).parent();
     
-    console.log($container)
-
+    console.log(WORKS)
     WORKS.sort(function(a, b) {
       return (Date.parse(new Date(a.date)) > Date.parse(new Date(b.date))) ? 1 : -1;
-    }).
-    forEach(function(work) {
+    })
+    console.log(WORKS)
+    WORKS.forEach(function(work) {
       $container.prepend($works.filter('[title="' + work.title + '"]'));
       console.log(work, $works.filter('[title="' + work.title + '"]'))
     });
